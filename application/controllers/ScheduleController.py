daef120cd548dfa23ef1d6a2df3b93661e3e13f8
Squidysquid1 <46660298+Squidysquid1@users.noreply.github.com>
2024-10-28 21:02:41 -0500

    add more test data

--- application/controllers/ScheduleController.py
+++ application/controllers/ScheduleController.py
@@ -21,6 +21,27 @@ class ScheduleController:
                         dict(time="6:00 PM - 7:00 PM", title="Dinner", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")]
 
         days.append(day2Schedule)
+
+        day3Schedule = [dict(time="9:00 AM - 10:00 AM", title="Something else", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="10:00 AM - 11:00 AM", title="Explore City", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="6:00 PM - 7:00 PM", title="Dinner", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")]
+
+        days.append(day3Schedule)
+
+        day4Schedule = [dict(time="9:00 AM - 10:00 AM", title="Something else", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="10:00 AM - 11:00 AM", title="Explore City", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="11:00 AM - 12:30 PM", title="Get rental car", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="12:30 PM - 1:30 PM", title="Lunch break", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="6:00 PM - 7:00 PM", title="Dinner", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")]
+
+        days.append(day4Schedule)
+        day5Schedule = [dict(time="9:00 AM - 10:00 AM", title="Something else", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="10:00 AM - 11:00 AM", title="Explore City", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="11:00 AM - 12:30 PM", title="Get rental car", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="12:30 PM - 1:30 PM", title="Lunch break", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                        dict(time="6:00 PM - 7:00 PM", title="Dinner", information="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")]
+
+        days.append(day5Schedule)
         
         data["days"] = days
 
