c7bc1b6328942e17aaa05fa1896ab22d0813b888
squidysquid1 <squid@squidysquid1.com>
2024-11-01 14:25:49 -0500

    clean up router code

--- application/routes/ApplicationRouter.py
+++ application/routes/ApplicationRouter.py
@@ -5,18 +5,25 @@ from application.controllers.FaqController import FaqController
 from application.controllers.CityPlanController import CityPlanController
 from application.controllers.ItineraryController import ItineraryController
 
+routes = []
+
 HomeRouter = Blueprint('home_controller', __name__)
 HomeRouter.route('/', methods=['GET'])(HomeController.index)
+routes.append(HomeRouter)
 
 ScheduleRouter = Blueprint('schedule_controller', __name__)
 ScheduleRouter.route('/schedule', methods=['GET'])(ScheduleController.index)
+routes.append(ScheduleRouter)
 
 FaqRouter = Blueprint('faq_controller', __name__)
 FaqRouter.route('/faq', methods=['GET'])(FaqController.index)
+routes.append(FaqRouter)
 
 CityPlanRouter = Blueprint('cityplan_controller', __name__)
 CityPlanRouter.route('/plan', methods=['GET'])(CityPlanController.index)
+routes.append(CityPlanRouter)
 
 ItineraryRouter = Blueprint('itinerary_controller', __name__)
 ItineraryRouter.route('/view-itinerary', methods=['GET'])(ItineraryController.index)
 ItineraryRouter.route('/create-itinerary', methods=['POST'])(ItineraryController.createItinerary)
+routes.append(ItineraryRouter)
--- framework/Router.py
+++ framework/Router.py
@@ -9,22 +9,8 @@ class Router:
         from . import Auth
         app.register_blueprint(Auth.bp)
 
-        #Home
-        from application.routes.ApplicationRouter import HomeRouter
-        app.register_blueprint(HomeRouter, url_prefix="/")
+        # Application routes
+        from application.routes.ApplicationRouter import routes
 
-        #Schedule
-        from application.routes.ApplicationRouter import ScheduleRouter
-        app.register_blueprint(ScheduleRouter, url_prefix="/")
-
-        #Faq
-        from application.routes.ApplicationRouter import FaqRouter
-        app.register_blueprint(FaqRouter, url_prefix="/")
-
-        #Plan
-        from application.routes.ApplicationRouter import CityPlanRouter
-        app.register_blueprint(CityPlanRouter, url_prefix="/")
-
-        from application.routes.ApplicationRouter import ItineraryRouter
-        app.register_blueprint(ItineraryRouter, url_prefix="/")
-        
+        for route in routes:
+            app.register_blueprint(route)
